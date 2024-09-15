4caa45e9d065286d31cf4b0f06d9ed2f9748fbd6
Piyush Agrawal <71814958+Piyush132000@users.noreply.github.com>
2024-09-15 04:32:10 +0000

    mongo db connection

---
+++ database/database.js
@@ -0,0 +1,27 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://piyush132000:password123@123#@cluster-1.bre18.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-1";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+module.exports = run;
